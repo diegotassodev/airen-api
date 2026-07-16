6a1525f7efce177f8993651c739a85b7e8657164
Diego Tasso <108370404+diegotassodev@users.noreply.github.com>
2026-07-16 17:13:38 -0300

    feat: restringir ação de exclusão de potentias

--- Data/AirenContext.cs
+++ Data/AirenContext.cs
@@ -8,4 +8,12 @@ public class AirenContext : DbContext {
     public AirenContext(DbContextOptions<AirenContext> opts) : base (opts) {}
     public DbSet<Personagem>? Personagens {get;set;}
     public DbSet<Potentia>? Potentias {get;set;}
+
+
+    protected override void OnModelCreating(ModelBuilder builder) {
+        builder.Entity<Personagem>()
+            .HasOne(personagem => personagem.Potentia)
+            .WithOne(potentia => potentia.Personagem)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
