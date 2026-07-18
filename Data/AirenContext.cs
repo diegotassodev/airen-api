045c8043a3faa0a0e110277c5847a07536f8d5ea
Diego Tasso <108370404+diegotassodev@users.noreply.github.com>
2026-07-18 19:09:53 -0300

    feat: adicionar novo contexto de dados baseado na nova modelagem

--- Data/AirenContext.cs
+++ Data/AirenContext.cs
@@ -8,12 +8,27 @@ public class AirenContext : DbContext {
     public AirenContext(DbContextOptions<AirenContext> opts) : base (opts) {}
     public DbSet<Personagem>? Personagens {get;set;}
     public DbSet<Potentia>? Potentias {get;set;}
-
+    public DbSet<Afiliacao>? Afiliacaos {get;set;}
+    public DbSet<Capitulo>? Capitulos {get;set;}
+    public DbSet<Habilidade>? Habilidades {get;set;}
+    public DbSet<Hobby>? Hobbies {get;set;}
+    public DbSet<Parentesco>? Parentescos {get;set;}
+    public DbSet<Tecnica>? Tecnicas {get;set;}
+    public DbSet<Versao>? Versoes {get;set;}
+    public DbSet<Volume>? Volumes {get;set;}
 
     protected override void OnModelCreating(ModelBuilder builder) {
-        builder.Entity<Personagem>()
-            .HasOne(personagem => personagem.Potentia)
-            .WithOne(potentia => potentia.Personagem)
+
+        builder.Entity<Parentesco>()
+            .HasOne(p => p.Personagem)
+            .WithMany()
+            .HasForeignKey(p => p.IdPersonagem)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Parentesco>()
+            .HasOne(p => p.Parente)
+            .WithMany()
+            .HasForeignKey(p => p.IdParente)
             .OnDelete(DeleteBehavior.Restrict);
     }
 }
